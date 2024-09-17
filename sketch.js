4bfac716e8449d441c77aea1262ac8978b5d4989
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-17 14:03:37 -0400

    top and bottoms added

--- sketch.js
+++ sketch.js
@@ -2,10 +2,26 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(40,25,0);
 
+  let rect1X = windowWidth/2 - 374;
+  let rect2X = windowWidth/2 - 108;
+  let rect3X = windowWidth/2 + 158;
+  let rectY = 50;
+
   fill(255,255,255);
-  rect(windowWidth/2 - 100, 50, 200, 650);
-  rect(windowWidth/2 - 350, 50, 200, 650);
-  rect(windowWidth/2 + 150, 50, 200, 650);
+  rect(rect1X, rectY, 216, 650);
+  rect(rect2X, rectY, 216, 650);
+  rect(rect3X, rectY, 216, 650);
+
+  rect(rect1X, rectY, 117, 5);
+  rect(rect1X + 117, rectY, 99, 5);
+  rect(rect2X, rectY, 108, 5);
+  rect(rect2X + 108, rectY, 108, 5);
+  rect(rect3X, rectY, 108, 5);
+  rect(rect3X + 108, rectY, 108, 5);
+
+  rect(rect1X, 650, 216, 50);
+  rect(rect2X, 650, 216, 50);
+  rect(rect3X, 650, 216, 50);
 
 }
 

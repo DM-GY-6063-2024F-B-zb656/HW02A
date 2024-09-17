4df7814ddeacd851d24067a1fe395c641ff3a89c
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-17 13:38:45 -0400

    made window outlines

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(40,25,0);
+
+  fill(255,255,255);
+  rect(windowWidth/2 - 100, 50, 200, 650);
+  rect(windowWidth/2 - 350, 50, 200, 650);
+  rect(windowWidth/2 + 150, 50, 200, 650);
+
 }
 
 function draw() {

c258119822f7d43601dcd3210b75d4fb85411339
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-17 14:12:25 -0400

    more borders

--- sketch.js
+++ sketch.js
@@ -12,16 +12,23 @@ function setup() {
   rect(rect2X, rectY, 216, 650);
   rect(rect3X, rectY, 216, 650);
 
-  rect(rect1X, rectY, 117, 5);
-  rect(rect1X + 117, rectY, 99, 5);
-  rect(rect2X, rectY, 108, 5);
-  rect(rect2X + 108, rectY, 108, 5);
-  rect(rect3X, rectY, 108, 5);
-  rect(rect3X + 108, rectY, 108, 5);
+  rect(rect1X, rectY, 117, 9);
+  rect(rect1X + 117, rectY, 99, 9);
+  rect(rect2X, rectY, 108, 9);
+  rect(rect2X + 108, rectY, 108, 9);
+  rect(rect3X, rectY, 108, 9);
+  rect(rect3X + 108, rectY, 108, 9);
 
   rect(rect1X, 650, 216, 50);
   rect(rect2X, 650, 216, 50);
   rect(rect3X, 650, 216, 50);
+
+  rect(rect2X, 641, 216, 9);
+  rect(rect2X, rectY+9, 9, 582);
+  rect(rect2X + 207, rectY+9, 9, 582);
+
+  rect(rect1X, rectY+9, 9, 591);
+  rect(rect1X + 207, rectY+9, 9, 591);
 
 }
 

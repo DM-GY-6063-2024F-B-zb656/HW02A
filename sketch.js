4edd0d2dbdadf469088f7b1e99a3ed6efc09d7be
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-18 21:15:16 -0400

    more boxes

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ function setup() {
   background(40,25,0);
 
   let rectX = windowWidth/2 - 120
-  let rectY = 50
+  let rectY = 25
 
   //main rectangle
-  rect(rectX, rectY, 240, 650);
+  rect(rectX, rectY, 240, 700);
 
   //top line
   rect(rectX, rectY, 120, 10);
@@ -30,11 +30,16 @@ function setup() {
   rect(rectX + 85, rectY + 90, 35, 30);
   rect(rectX + 85, rectY + 120, 35, 10);
   rect(rectX + 85, rectY + 130, 35, 10);
+  rect(rectX, rectY + 190, 10, 20);
+  rect(rectX + 30, rectY + 190, 45, 20);
+  rect(rectX, rectY + 210, 10, 10);
+  rect(rectX + 10, rectY + 210, 65, 10);
 
   //right side
   rect(rectX + 230, rectY + 10, 10, 20);
   rect(rectX + 230, rectY + 30, 10, 40);
   rect(rectX + 130, rectY + 30, 100, 40);
+  rect(rectX + 230, rectY + 80, 10, 130);
 
   //second line
   rect(rectX + 10, rectY + 10, 110, 10);
@@ -66,10 +71,14 @@ function setup() {
   rect(rectX + 220, rectY + 20, 10, 5);
   rect(rectX + 220, rectY + 25, 10, 5);
 
-  //middle vertical small boxes
+  //middle vertical line
   rect(rectX + 120, rectY + 40, 10, 10);
   rect(rectX + 120, rectY + 60, 10, 10);
   rect(rectX + 120, rectY + 80, 10, 10);
+  rect(rectX + 120, rectY + 90, 10, 30);
+  rect(rectX + 120, rectY + 120, 10, 10);
+  rect(rectX + 120, rectY + 130, 10, 10);
+  rect(rectX + 120, rectY + 140, 10, 40);
 
   //left small boxes
   rect(rectX + 10, rectY + 110, 10, 10);
@@ -92,10 +101,11 @@ function setup() {
   rect(rectX + 55, rectY + 180, 10, 10);
   rect(rectX + 75, rectY + 180, 10, 10);
   rect(rectX + 75, rectY + 200, 10, 10);
-
-
-
-
+  rect(rectX + 95, rectY + 180, 15, 10);
+  rect(rectX + 120, rectY + 180, 10, 10);
+  rect(rectX + 85, rectY + 190, 15, 10);
+  rect(rectX + 85, rectY + 200, 25, 10);
+  rect(rectX + 120, rectY + 200, 10, 10);
 
 
   // colored boxes down here v
@@ -115,11 +125,9 @@ function setup() {
   rect(rectX + 75, rectY + 190, 10, 10);
   rect(rectX + 75, rectY + 210, 10, 10);
 
-
-
-
   fill(8,9,201); //dark blue
   rect(rectX + 40, rectY + 20, 10, 10);
+  rect(rectX + 120, rectY + 210, 10, 10);
 
   fill(219,155,0); //dark orange
   rect(rectX + 100, rectY + 20, 10, 10);
@@ -134,22 +142,35 @@ function setup() {
   rect(rectX + 10, rectY + 160, 10, 10);
   rect(rectX + 20, rectY + 170, 10, 10);
 
-
   fill(0,166,181); //light blue
   rect(rectX + 140, rectY + 20, 10, 10);
 
-  fill(194,0,0); //brighter red
-  rect(rectX + 180, rectY + 20, 10, 10);
-
   fill(173,0,0); //dark red
+  rect(rectX + 180, rectY + 20, 10, 10);
   rect(rectX + 210, rectY + 20, 10, 10);
   rect(rectX + 30, rectY + 60, 45, 10);
+  rect(rectX + 75, rectY + 160, 10, 10);
+  rect(rectX + 110, rectY + 180, 10, 10);
+  rect(rectX + 100, rectY + 190, 10, 10);
+  rect(rectX + 120, rectY + 190, 10, 10);
+  rect(rectX + 110, rectY + 200, 10, 10);
 
   fill(247,193,0); //marigold
   rect(rectX + 10, rectY + 70, 20, 20);
 
   fill(70,102,43); //dark olive
   rect(rectX + 120, rectY + 30, 10, 10);
+
+  fill(97, 173, 30) //light green
+  rect(rectX + 10, rectY + 190, 20, 20);
+
+
+  //circles
+  fill(173,0,0); //dark red
+  ellipse(rectX + 75, rectY + 65, 60);
+
+  fill(247,193,0); //marigold
+  ellipse(rectX + 85, rectY + 135, 60);
 
 }
 

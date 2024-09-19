7385c3e4b094bf469564d0dcf2b89fb0049b3de0
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-18 20:46:48 -0400

    more boxes

--- sketch.js
+++ sketch.js
@@ -25,6 +25,11 @@ function setup() {
   rect(rectX + 75, rectY + 60, 45, 10);
   rect(rectX + 30, rectY + 70, 45, 20);
   rect(rectX + 75, rectY + 70, 45, 20);
+  rect(rectX + 30, rectY + 90, 45, 40);
+  rect(rectX + 75, rectY + 90, 10, 40);
+  rect(rectX + 85, rectY + 90, 35, 30);
+  rect(rectX + 85, rectY + 120, 35, 10);
+  rect(rectX + 85, rectY + 130, 35, 10);
 
   //right side
   rect(rectX + 230, rectY + 10, 10, 20);
@@ -64,6 +69,31 @@ function setup() {
   //middle vertical small boxes
   rect(rectX + 120, rectY + 40, 10, 10);
   rect(rectX + 120, rectY + 60, 10, 10);
+  rect(rectX + 120, rectY + 80, 10, 10);
+
+  //left small boxes
+  rect(rectX + 10, rectY + 110, 10, 10);
+  rect(rectX + 20, rectY + 110, 10, 10);
+  rect(rectX, rectY + 120, 10, 10);
+  rect(rectX + 20, rectY + 120, 10, 10);
+  rect(rectX + 10, rectY + 130, 10, 10);
+  rect(rectX + 20, rectY + 140, 10, 10);
+  rect(rectX + 20, rectY + 160, 10, 10);
+  rect(rectX, rectY + 170, 10, 10);
+  rect(rectX + 10, rectY + 170, 10, 10);
+  rect(rectX + 10, rectY + 180, 10, 10);
+  rect(rectX + 30, rectY + 170, 5, 20);
+  rect(rectX + 35, rectY + 170, 10, 10);
+  rect(rectX + 45, rectY + 170, 10, 10);
+  rect(rectX + 55, rectY + 170, 10, 10);
+  rect(rectX + 65, rectY + 170, 10, 10);
+  rect(rectX + 75, rectY + 170, 10, 10);
+  rect(rectX + 35, rectY + 180, 10, 10);
+  rect(rectX + 55, rectY + 180, 10, 10);
+  rect(rectX + 75, rectY + 180, 10, 10);
+  rect(rectX + 75, rectY + 200, 10, 10);
+
+
 
 
 
@@ -76,12 +106,34 @@ function setup() {
   rect(rectX + 160, rectY + 20, 10, 10);
   rect(rectX + 230, rectY + 70, 10, 10);
   rect(rectX + 120, rectY + 50, 10, 10);
+  rect(rectX + 120, rectY + 70, 10, 10);
+  rect(rectX, rectY + 180, 10, 10);
+  rect(rectX + 20, rectY + 180, 10, 10);
+  rect(rectX + 45, rectY + 180, 10, 10);
+  rect(rectX + 65, rectY + 180, 10, 10);
+  rect(rectX + 85, rectY + 180, 10, 10);
+  rect(rectX + 75, rectY + 190, 10, 10);
+  rect(rectX + 75, rectY + 210, 10, 10);
+
+
+
 
   fill(8,9,201); //dark blue
   rect(rectX + 40, rectY + 20, 10, 10);
 
   fill(219,155,0); //dark orange
   rect(rectX + 100, rectY + 20, 10, 10);
+  rect(rectX + 200, rectY + 70, 30, 30);
+  rect(rectX, rectY + 110, 10, 10);
+  rect(rectX + 10, rectY + 120, 10, 10);
+  rect(rectX, rectY + 130, 10, 10);
+  rect(rectX + 20, rectY + 130, 10, 10);
+  rect(rectX + 10, rectY + 140, 10, 10);
+  rect(rectX, rectY + 150, 10, 10);
+  rect(rectX + 20, rectY + 150, 10, 10);
+  rect(rectX + 10, rectY + 160, 10, 10);
+  rect(rectX + 20, rectY + 170, 10, 10);
+
 
   fill(0,166,181); //light blue
   rect(rectX + 140, rectY + 20, 10, 10);
